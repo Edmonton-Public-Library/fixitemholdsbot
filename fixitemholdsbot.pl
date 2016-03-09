8a968540a86cc8ffd3c0880733da3f12a6784052
Andrew Nisbet <anisbet@epl.ca>
2016-03-08 17:01:24 -0700

    Test all but -U to do the actual editholds.

--- fixitemholdsbot.pl
+++ fixitemholdsbot.pl
@@ -54,6 +54,48 @@ my $BINCUSTOM          = `getpathname bincustom`;
 chomp $BINCUSTOM;
 my $PIPE               = "$BINCUSTOM/pipe.pl";
 
+my @INVALID_LOCATIONS  = qw{
+UNKNOWN 
+REFERENCE
+MISSING
+LOST
+BINDERY
+INPROCESS
+DISCARD
+ILL
+RESERVES
+CATALOGING
+LOST-PAID
+REPAIR
+AVCOLL
+CANC_ORDER
+DISPLAY
+EPLBINDERY
+EPLCATALOG
+EPLILL
+GOVPUB
+FLICKTUNE
+INCOMPLETE
+INDEX
+INTERNET
+STORAGE
+STORAGEHER
+STORAGEREF
+DAMAGE
+BARCGRAVE
+NON-ORDER
+LOST-ASSUM
+LOST-CLAIM
+PROGRAM
+BESTSELLER
+REF-ORDER
+JBESTSELLR
+STORAGEGOV
+INSHIPPING
+STOLEN
+NOF
+MAKER};
+
 #
 # Message about this program and how to use it.
 #
@@ -62,9 +104,10 @@ sub usage()
     print STDERR << "EOF";
 
 	usage: $0 [-xt]
-Usage notes for deleteme.pl.
+Usage notes for $0.pl.
 
  -t: Preserve temporary files in $TEMP_DIR.
+ -U: Do the work, otherwise just print what would do to STDERR.
  -x: This (help) message.
 
 example:
@@ -119,12 +162,55 @@ sub create_tmp_file( $$ )
 	return $master_file;
 }
 
+# Tests if an input of item id and location is a valid location according to the
+# list of valid locations in the getpol -tLOCN tool.
+# param:  line like '12345|1|2|CHECKEDOUT|'
+# return: 1 if the location is valid and 0 otherwise.
+sub isValidLocation( $ )
+{
+	# TODO: Finish me.
+	my $catKeyLocation = shift;
+	my @fields = split '\|', $catKeyLocation;
+	if ( defined $fields[3] )
+	{
+		foreach my $location ( @INVALID_LOCATIONS )
+		{
+			return 0 if ( $fields[3] =~ m/($location)/ ); # if the item's location matches one of the forbidden locations return false.
+		}
+	}
+	else
+	{
+		return 0;
+	}
+	return 1;
+}
+
+# Gets a viable item id from all of the item ids on a title. 
+# param:  cat key
+# param:  sequence number
+# param:  copy number of the offending item.
+# return: triple of new cat key, sequence number, and copy number of a good copy
+#         from the title or an empty triple if none was found.
+sub get_viable_itemKey( $$$ )
+{
+	my ( $ck, $sn, $cn ) = @_;
+	my ( $newCK, $newSN, $newCN ) = "";
+	$newCK = $ck;
+	my $results = `echo "$ck" | selitem -iC -oIl 2>/dev/null`;
+	my @resultLines = split '\n', $results;
+	foreach my $line ( @resultLines )
+	{
+		( $newCK, $newSN, $newCN ) = split '\|', $line if ( isValidLocation( $line ) );
+	}
+	return ( $newCK, $newSN, $newCN );
+}
+
 # Kicks off the setting of various switches.
 # param:  
 # return: 
 sub init
 {
-    my $opt_string = 'tx';
+    my $opt_string = 'tUx';
     getopts( "$opt_string", \%opt ) or usage();
     usage() if ( $opt{'x'} );
 }
@@ -155,7 +241,38 @@ my $itemIdFile = create_tmp_file( "fixitemdb_", $results );
 $results = `cat "$itemIdFile" | "$PIPE" -d'c0,c1,c2' -P`;
 my $dedupItemIdFile = create_tmp_file( "fixitemdb_", $results );
 # TODO: Using the catalog key(s) find a valid item.
-
+# Open the $dedupItemIdFile file and use the cat key to get all the item keys.
+# We will use the locations to determine viable copies and then choose the 
+# sequence number and copy number (if necessary) from a viable item with a reasonable location.
+open ITEM_KEYS, "<$dedupItemIdFile" || die "** error opening '$dedupItemIdFile', $!.\n";
+while (<ITEM_KEYS>)
+{
+	my ( $catKey, $seqNumber, $copyNumber ) = split '\|', $_;
+	my ( $viableItemKey, $viableSeqNumber, $viableCopyNumber ) = get_viable_itemKey( $catKey, $seqNumber, $copyNumber );
+	if ( $viableItemKey eq "" )
+	{
+		printf STDERR "* warning: no viable items on cat key '%s'.\n", $catKey;
+		next;
+	}
+	# Now replace the old sequence with the new, and if required also the copy number.
+	printf STDERR "item key '%s|%s|%s|'.\n", $viableItemKey, $viableSeqNumber, $viableCopyNumber;
+	my $newItemId = sprintf "%s|%s|%s|", $viableItemKey, $viableSeqNumber, $viableCopyNumber;
+	# Get a list of all the effected item ids for this title by sequence and copy number.
+	$results = `echo "$catKey" | selhold -iC -c"$seqNumber" -d"$copyNumber" -oKIja`;
+	# 21683010|1216974|4|3|ACTIVE|N|
+	my $holdsToFix = create_tmp_file( "fixitemdb_$catKey", $results );
+	if ( $opt{'U'} )
+	{
+		# Now edit the hold to the new sequence number and copy number.
+		`cat "$holdsToFix" | edithold -c"$viableSeqNumber"`;
+		`cat "$holdsToFix" | edithold -d"$viableCopyNumber"`;
+	}
+	else
+	{
+		printf STDERR "cat \"%s\" | edithold -c\"%s\"\n", $holdsToFix, $viableSeqNumber;
+		printf STDERR "cat \"%s\" | edithold -d\"%s\"\n", $holdsToFix, $viableCopyNumber;
+	}
+}
 ### code ends
 if ( $opt{'t'} )
 {
